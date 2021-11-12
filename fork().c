195a7bb3fca1941c48d0b92d7f4b04b176820b04
pavanivepati <85747124+pavanivepati@users.noreply.github.com>
2021-11-12 14:44:20 +0530

    Create fork().c

---
+++ fork().c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+int main()
+{
+    fork();
+    printf("WELCOME TO ADITYA ENGINEERING COLLEGE\n");
+    return 0;
+}
+
+
+output:
+WELCOME TO ADITYA ENGINEERING COLLEGE
+WELCOME TO ADITYA ENGINEERING COLLEGE
